e0e5f53559574e31cdc0809cc37e3ebbb10888ac
Serhii Ostrikov <serostrikov@gmail.com>
2023-10-17 13:02:39 +0300

    Added output (+ a little for loop) example

---
+++ examples/terraform/output-and-for-loop/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "hello_variables" {
+  ami = var.ami
+  instance_type = var.instance_type 
+  tags = var.funny_tag
+  count = var.instance_count
+}
+
+resource "aws_iam_user" "iam_users" {
+  count = length(var.user_names)
+  name = var.user_names[count.index]
+  
+  tags = {
+    Number = "${local.funny_prefix}-user${count.index + 1}"
+  }
+}
---
+++ examples/terraform/output-and-for-loop/output.tf
@@ -0,0 +1,6 @@
+output "machine_public_ip" {
+  value = [for i in aws_instance.hello_variables: i.public_ip]
+  # same thing i guess
+  # value = aws_instance.hello_variables[*].public_ip
+  sensitive = true # use terraform output -raw machine_public_ip
+}
