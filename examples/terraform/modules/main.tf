8154cc6266b8b87bab84008fa0f18563ff727872
Serhii Ostrikov <serostrikov@gmail.com>
2023-10-19 23:32:31 +0300

    Added modules example

---
+++ examples/terraform/modules/main.tf
@@ -0,0 +1,15 @@
+
+provider "aws" {
+  region = var.region
+}
+
+module "funny_ec2" {
+  source = "./module1"
+  ssh_public_key_file = var.ssh_public_key_file
+  ssh_private_key_file = var.ssh_private_key_file
+}
+
+output "public_ip" {
+  value = module.funny_ec2.ec2_public_ip
+  sensitive = true
+}
