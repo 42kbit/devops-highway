3e788af8b6325c68447397707ea6a739f2463f12
Serhii Ostrikov <serostrikov@gmail.com>
2023-10-20 23:28:14 +0300

    Extended templates example with templatefile function

--- examples/terraform/templates/main.tf
+++ examples/terraform/templates/main.tf
@@ -11,3 +11,15 @@ data "template_file" "policy" {
 output "policy" {
   value = "This is templated value: ${data.template_file.policy.rendered}"
 }
+
+# heres another example
+
+locals {
+  rendered = templatefile("${path.module}/file.tpl", {
+    name = "another_deb"
+  })
+}
+
+output "policy_by_templatefile" {
+  value = "This is templatefile value: ${local.rendered}"
+}
