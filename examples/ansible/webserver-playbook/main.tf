442bb046a583636c2e390401057a35c5fd1b8a4e
Serhii Ostrikov <serostrikov@gmail.com>
2023-10-22 19:19:48 +0300

    Added ansible webserver example

---
+++ examples/ansible/webserver-playbook/main.tf
@@ -0,0 +1,109 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1"
+}
+
+variable "ansible_pub_key_path" {}
+variable "ansible_pri_key_path" {}
+variable "instance_count" {}
+
+resource "aws_instance" "hello_world" {
+  count = var.instance_count
+  ami = "ami-0ea7dc624e77a15d5"
+  instance_type = "t3.micro"
+  key_name = aws_key_pair.hello_world.key_name
+  vpc_security_group_ids = [aws_security_group.hello_world.id]
+
+  depends_on = [aws_security_group.hello_world, aws_key_pair.hello_world]
+}
+
+resource "local_file" "ansible_inventory" {
+  filename = "${path.module}/inventory"
+  content = templatefile("${path.module}/inventory.tpl", {
+    public_ips = aws_instance.hello_world[*].public_ip
+  })
+}
+
+resource "aws_key_pair" "hello_world" {
+  key_name = "ansible_pub_key"
+  public_key = file(var.ansible_pub_key_path)
+}
+
+resource "aws_security_group" "hello_world" {
+  name        = "hello_world_allow_ssh"
+  description = "Allows ssh for ansible"
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  ingress {
+    description      = "ssh from vpc"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "http for vpc"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+resource "null_resource" "configure_ansible" {
+  count = var.instance_count
+
+  triggers = {
+    # run everytime
+    run_everytime = timestamp()
+  }
+
+  # this shit will ensure that connection can be established
+  # before starting ansible. More:
+  # https://stackoverflow.com/questions/62403030/terraform-wait-till-the-instance-is-reachable
+  provisioner "remote-exec" {
+    connection {
+      host = aws_instance.hello_world[count.index].public_ip
+      user = "ec2-user"
+      private_key = file(var.ansible_pri_key_path)
+    }
+
+    inline = [ "echo Connection can be established, starting ansible..." ]
+  }
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      ANSIBLE_HOST_KEY_CHECKING=False \
+      ansible all --key-file ${var.ansible_pri_key_path} \
+      -i ${local_file.ansible_inventory.filename} \
+      -m ping --user ec2-user;
+
+      ansible-playbook --key-file ${var.ansible_pri_key_path} \
+      -i ${local_file.ansible_inventory.filename} \
+      --user ec2-user apache_init.yml
+    EOT
+  }
+}
